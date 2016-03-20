78bf647fd08816a4be54dccf4957d44f18106391
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2016-03-20 03:03:33 +0100

    Cena enhancement

--- www/js/custom.js
+++ www/js/custom.js
@@ -5,4 +5,9 @@ $(document).ready(function() {
     $("figure").on('touchstart', function(){
         $(this).get(0).hover();
     });
+    
+    $('#partecipa').on('click', function () {
+        $("#img-cena").attr("src", "images/CENA2.png");
+        window.scrollTo(0, 0);
+    })
 });
