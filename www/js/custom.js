28e590a7b0027f7916080cad7e831ea053836bca
federicoB <federicobertani96@gmail.com>
2016-03-20 06:32:49 +0100

    Little change in name

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,13 +1,8 @@
 /**
  * Created by MatteoPellegrino on 20/03/16.
  */
-$(document).ready(function() {
-    $("figure").on('touchstart', function(){
-        $(this).get(0).hover();
-    });
     
     $('#partecipa').on('click', function () {
         $("#img-cena").attr("src", "images/CENA2.png");
        // window.scrollTo(0, 0);
-    })
-});
+    });
