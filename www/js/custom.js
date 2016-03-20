9c20d4b1c83525c73f497940e5d83cf1f4d444db
FedeBer <fedegeek@gmail.com>
2016-03-20 07:25:21 +0100

    Fixed javascrip

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,8 +1,10 @@
 /**
  * Created by MatteoPellegrino on 20/03/16.
  */
+$(document).ready(function() {
     
     $('#partecipa').on('click', function () {
         $("#img-cena").attr("src", "images/CENA2.png");
        // window.scrollTo(0, 0);
-    });
+    })
+});
